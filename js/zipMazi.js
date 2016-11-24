de8ce257d0ab9a7953e4f863f33241b46d06ea4d
Yesterday17 <t@yesterday17.cn>
2016-11-24 20:14:58 +0800

    修改zipMazi.js，完成index.html中关于文件上载的部分。

--- js/zipMazi.js
+++ js/zipMazi.js
@@ -4,27 +4,40 @@
  * 3.jszip-utils.js
 */
 
+var maziUtils = function(){
+    var splitZipFilename = function(absName){
+        return absName.split("/");
+    }
+}
+
 function getFile(zipName, fileName, asyncType) {
+    var ans;
+
     JSZip.loadAsync(zipName)
         .then(function (zip) {
-            zip.forEach(function (relativePath, zipEntry) {
-                try {
-                    if (zipEntry.name == fileName) {
+            var arr = zip.files;
+            console.log(arr);
+            for (var zipEntry in arr) {
+                if(zipEntry.dir != true) {
+                    console.log(zipEntry);
+                    if (zipEntry.name.split("/")[1] == fileName) {
                         zip.file(zipEntry.name).async(asyncType)
-                            .then(function success(content) {
-                                //Add success code here.
-                                return content;
-                            }, function error(e) {
-                                //Add error code here.
-                                return "{\"type\" : \"error\", \"message\" : \"" + e.message + "\"}";
-                            });
+                        .then(function success(content) {
+                            //Add success code here.
+                            ans = content;
+                        }, function error(e) {
+                            //Add error code here.
+                            ans = "{\"type\" : \"error\", \"message\" : \"" + e.message + "\"}";
+                        });
+                    }
+                    else{
+                        ans = "{\"type\" : \"error\", \"message\" : \"" + "e.message" + "\"}";
                     }
                 }
-                catch (e) {
-                    return "{\"type\" : \"error\", \"message\" : \"" + e.message + "\"}";
-                }
-            });
+            };
         }, function (e) {
-            return "{\"type\" : \"error\", \"message\" : \"" + e.message + "\"}";
-        });
+            ans = "{\"type\" : \"error\", \"message\" : \"" + e.message + "\"}";
+        })
+        .done();
+        return ans;
 }
